4af58b278adb91935887667f391b41ef06ec49e3
Priyansh Soni <spriyansh633@gmail.com>
2025-09-07 12:07:07 +0530

    Fix src folder, add all files

---
+++ src/components/NewsItem.js
@@ -0,0 +1,42 @@
+import React from "react";
+
+const NewsItem = (props) => {
+  let { title, description, imageUrl, newsUrl, author, date, source } = props;
+  return (
+    <div className="my-3">
+      <div className="card">
+        <span className="badge text-bg-danger">{source}</span>
+        <img
+          src={
+            !imageUrl
+              ? "https://lh3.googleusercontent.com/J6_coFbogxhRI9iM864NL_liGXvsQp2AupsKei7z0cNNfDvGUmWUy20nuUhkREQyrpY4bEeIBuc=s0-w300-rw"
+              : imageUrl
+          }
+          className="card-img-top"
+          alt="..."
+        />
+        <div className="card-body">
+          <h5 className="card-title">{title}</h5>
+          <p className="card-text">{description}</p>
+          <p className="card-text">
+            <small className="text-body-secondary">
+              By {!author ? "Unknown" : author} on{" "}
+              {new Date(date).toGMTString()}
+            </small>
+          </p>
+          <a
+            rel="noreferrer"
+            href={newsUrl}
+            target="_blanck"
+            className="btn btn-sm  btn-dark"
+          >
+            {" "}
+            Read more{" "}
+          </a>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default NewsItem;
